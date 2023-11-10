e007abaf9397a9c1b1032b1601c0a9f5c537010f
ViktorChizh <victor.chizh@mail.ru>
2023-11-10 20:53:22 +0300

    docs

--- src/components/burgerMenu/BurgerMenu.tsx
+++ src/components/burgerMenu/BurgerMenu.tsx
@@ -5,7 +5,7 @@ import { Icon } from '../icon/Icon';
 export const BurgerMenu = () => {
     return (
         <>
-            <Icon iconId='еудупкфь' width='50' height='50' viewport='0 0 75 75' />
+            <Icon iconId='code' width='50' height='50' viewport='0 0 50 50' />
         </>
     )
 }
