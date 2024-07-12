b7250d26afe43d70e4989a4c1b34929b273a88f4
ViktorChizh <victor.chizh@mail.ru>
2024-07-12 09:39:59 +0300

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
