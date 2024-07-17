38080cd80dbef7c5ddf057905fdecffbdea90ced
ViktorChizh <victor.chizh@mail.ru>
2024-07-17 09:43:29 +0300

    refactor

--- src/stylesAndThemes/Globalstyles.ts
+++ src/stylesAndThemes/Globalstyles.ts
@@ -15,40 +15,45 @@ export const GlobalStyles = createGlobalStyle`
         scrollbar-color: rgb(200, 225, 250) rgb(150, 200, 250);
     }
   }
-  *::-webkit-scrollbar-track{
-      background-color: rgb(150, 200, 250);
-  }  
-  *::-webkit-scrollbar {
-    width: 10px;
+  * {
+      &::-webkit-scrollbar-track {
+          background-color: rgb(150, 200, 250);
+      }
+
+      &::-webkit-scrollbar {
+          width: 10px;
+      }
+
+      &::-webkit-scrollbar:horizontal {
+          height: 10px;
+      }
+
+      &::-webkit-scrollbar-thumb {
+          background-color: #7572D5;
+          border: 3px solid rgb(150, 200, 250);
+          border-radius: 10px;
+      }
+
+      &::-webkit-scrollbar-thumb:horizontal {
+          background-color: #7572D5;
+          border: 3px solid rgb(150, 200, 250);
+          border-radius: 10px;
+      }
   }
-  *::-webkit-scrollbar:horizontal {
-      height: 10px;
-  }
-  *::-webkit-scrollbar-thumb{
-    background-color: #7572D5;
-    border: 3px solid rgb(150, 200, 250);
-    border-radius: 10px;
-  }
-  *::-webkit-scrollbar-thumb:horizontal{
-      background-color: #7572D5;
-      border: 3px solid rgb(150, 200, 250);
-      border-radius: 10px;
-  }
-  
-  body {
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-      sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    min-width: 430px;
-    }
-  
-  a {
-    text-decoration: none;
-  }
-  li {
-      list-style: none;
-  }
+      body {
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+          'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+          sans-serif;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+          min-width: 430px;
+      }
+
+      a {
+          text-decoration: none;
+      }
+      li {
+          list-style: none;
+      }
 `
