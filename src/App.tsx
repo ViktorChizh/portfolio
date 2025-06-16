cc2e4b96d59683217506c2b523470280b078369e
ViktorChizh <victor.chizh@mail.ru>
2025-06-16 08:49:32 +0300

    update skills

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,37 @@
-import styled, { ThemeProvider } from 'styled-components'
-import { Header } from 'layout/header/Header'
-import { Home } from 'layout/home/Home'
-import { Skills } from 'layout/skills/Skills'
-import { Works } from 'layout/works/Works'
-import { Contact } from 'layout/contact/Contact'
-import { Footer } from 'layout/footer/Footer'
-import { Quotes } from 'layout/quotes/Quotes'
-import { Theme } from 'stylesAndThemes/Theme'
-import { GlobalStyles } from 'stylesAndThemes/Globalstyles'
-import { Particle } from 'components/Particle'
+import styled, { ThemeProvider } from "styled-components";
+import { Header } from "layout/header/Header";
+import { Home } from "layout/home/Home";
+import { Skills } from "layout/skills/Skills";
+import { Works } from "layout/works/Works";
+import { Contact } from "layout/contact/Contact";
+import { Footer } from "layout/footer/Footer";
+import { Quotes } from "layout/quotes/Quotes";
+import { Theme } from "stylesAndThemes/Theme";
+import { GlobalStyles } from "stylesAndThemes/Globalstyles";
+import { Particle } from "components/Particle";
 
 function App() {
-    return (
-        <ThemeProvider theme={Theme}>
-            <StyledApp>
-                <Particle />
-                <Header/>
-                <Home/>
-                <Skills/>
-                <Quotes/>
-                <Works/>
-                <Contact/>
-                <Footer/>
-            </StyledApp>
-            <GlobalStyles/>
-        </ThemeProvider>
-
-    );
+  return (
+    <ThemeProvider theme={Theme}>
+      <StyledApp>
+        <Particle />
+        <Header />
+        <Home />
+        <Skills />
+        <Quotes />
+        <Works />
+        <Contact />
+        <Footer />
+      </StyledApp>
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 }
 
 export default App;
 
 const StyledApp = styled.main`
-    box-sizing: border-box;
-    width: 100%;
-    margin: 0 auto;
-`
+  box-sizing: border-box;
+  width: 100%;
+  margin: 0 auto;
+`;
