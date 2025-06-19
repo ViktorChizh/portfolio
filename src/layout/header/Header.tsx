419edd1f8822b54ca1ae90bfd0c76d572cfd3da4
ViktorChizh <victor.chizh@mail.ru>
2025-06-19 12:08:48 +0300

    update

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,39 +1,57 @@
-import React from 'react'
-import { Menu } from 'components/Menu'
-import styled from 'styled-components'
-import { Theme } from 'stylesAndThemes/Theme'
-import { Container } from 'components/Container'
-import { Icon } from 'components/Icon'
-import { Button } from 'components/Button'
-import { animateScroll } from 'react-scroll'
+import React from "react";
+import { Menu } from "components/Menu";
+import styled from "styled-components";
+import { Theme } from "stylesAndThemes/Theme";
+import { Container } from "components/Container";
+import { Icon } from "components/Icon";
+import { Button } from "components/Button";
+import { animateScroll } from "react-scroll";
 
 export const Header = () => {
-    return (
-        <StyledHeader>
-            <Container>
-                <Button border='none' bgColor='transparent' height='50px' onClick={()=>{animateScroll.scrollToTop()}}>
-                    <Icon iconId='react2' viewport='0 0 125 125'/>
-                </Button>
-                <Menu items={['Home', 'Skills', 'Quotes', 'Works', 'Contact']}  color={'rgb(200, 225, 250)'}/>
-                <Button border='none' bgColor='transparent' height='50px' onClick={()=>{animateScroll.scrollToTop()}}>
-                    <Icon iconId='redux2' viewport='0 0 125 125'/>
-                </Button>
-            </Container>
-        </StyledHeader>
-    )
-}
+  return (
+    <StyledHeader>
+      <Container>
+        <Button
+          border="none"
+          bgColor="transparent"
+          height="50px"
+          onClick={() => {
+            animateScroll.scrollToTop();
+          }}
+        >
+          <Icon iconId="react2" viewport="0 0 125 125" />
+        </Button>
+        <Menu
+          items={["Home", "Skills", "Quotes", "Works", "Contact"]}
+          color={"rgb(200, 225, 250)"}
+        />
+        <Button
+          border="none"
+          bgColor="transparent"
+          height="50px"
+          onClick={() => {
+            animateScroll.scrollToTop();
+          }}
+        >
+          <Icon iconId="redux2" viewport="0 0 125 125" />
+        </Button>
+      </Container>
+    </StyledHeader>
+  );
+};
 
 const StyledHeader = styled.header`
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    width: 100%;
-    z-index: 99;
-    background-color: ${Theme.colors.bgDark};
-    ${Container} {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 5px;
-`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  z-index: 99;
+  background-color: ${Theme.colors.bgDark};
+  ${Container} {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 5px;
+  }
+`;
--- src/layout/quotes/Quotes.tsx
+++ src/layout/quotes/Quotes.tsx
@@ -1,81 +1,119 @@
-import {Container} from 'components/Container';
-import {StyledTitle} from 'components/StyledTitle';
-import React from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
-import 'layout/quotes/Slider.css'
-import styled from 'styled-components';
-import {Theme} from 'stylesAndThemes/Theme';
-import quotes from '../../assets/quotes.svg'
+import { Container } from "components/Container";
+import { StyledTitle } from "components/StyledTitle";
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import "layout/quotes/Slider.css";
+import styled from "styled-components";
+import { Theme } from "stylesAndThemes/Theme";
+import quotes from "../../assets/quotes.svg";
 
 export const Quotes = () => {
   return (
-    <StyledQuotes id={'Quotes'}>
+    <StyledQuotes id={"Quotes"}>
       <Container>
         <StyledTitle>My Life Principles In Quotes</StyledTitle>
-        <AliceCarousel mouseTracking items={items} controlsStrategy="alternate"/>
+        <AliceCarousel
+          mouseTracking
+          items={items}
+          controlsStrategy="alternate"
+        />
       </Container>
     </StyledQuotes>
-  )
-}
+  );
+};
 
 const StyledQuotes = styled.section`
-    background-color: ${Theme.colors.bgPrimary};
-`
+  background-color: ${Theme.colors.bgPrimary};
+`;
 const Slider = styled.div`
-    display: flex;
-    width: 80%;
-    margin: 0 auto;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  width: 80%;
+  margin: 0 auto;
+  flex-direction: column;
+  align-items: center;
 
-    cite {
-        text-align: center;
-        color: ${Theme.colors.textTitle};
-        font-family: 'Comforter Brush', cursive;
-        font-size: 28px;
-        letter-spacing: 0.25em;
-        font-weight: 700;
-        width: 100%;
-        position: relative;
+  cite {
+    text-align: center;
+    color: ${Theme.colors.textTitle};
+    font-family: "Comforter Brush", cursive;
+    font-size: 28px;
+    letter-spacing: 0.25em;
+    font-weight: 700;
+    width: 100%;
+    position: relative;
 
-        ::after {
-            content: url(${quotes});
-            transform: rotate(180deg);
-            position: absolute;
-            right: -2%;
-            bottom: 1.5rem;
-        }
-
-        ::before {
-            content: url(${quotes});
-            position: absolute;
-            left: -2%;
-            top: 0;
-        }
+    ::after {
+      content: url(${quotes});
+      transform: rotate(180deg);
+      position: absolute;
+      right: -2%;
+      bottom: 1.5rem;
     }
 
-    strong {
-        margin-top: 10px;
-        align-self: flex-end;
-        color: ${Theme.colors.iconPrimary};
+    ::before {
+      content: url(${quotes});
+      position: absolute;
+      left: -2%;
+      top: 0;
     }
+  }
 
-    :hover {
-        cursor: grab;
-    }
+  strong {
+    margin-top: 10px;
+    align-self: flex-end;
+    color: ${Theme.colors.iconPrimary};
+  }
 
-    :active {
-        cursor: grabbing;
-    }
-`
+  :hover {
+    cursor: grab;
+  }
+
+  :active {
+    cursor: grabbing;
+  }
+`;
 
 const items = [
-  <Slider><cite>Делай все, что можешь, там, где ты находишься, используя все, что имеешь.</cite><strong>Теодор Рузвельт</strong></Slider>,
-  <Slider><cite>Единственный способ сделать что-то очень хорошо – любить то, что ты делаешь.</cite><strong>Стив Джобс</strong></Slider>,
-  <Slider><cite>Я не провалил тест. Я просто нашел сто способов написать его неправильно.</cite><strong>Бенджамин Франклин</strong></Slider>,
-  <Slider><cite>Выживает не самый сильный, а самый восприимчивый к переменам.</cite><strong>Чарльз Дарвин</strong></Slider>,
-  <Slider><cite>Вопрос не в том, кто мне разрешит, а в том, кто сможет мне запретить.</cite><strong>Айн Рэнд</strong></Slider>,
-  <Slider><cite>Неудача – это просто возможность начать снова, но уже более мудро.</cite><strong>Генри Форд</strong></Slider>,
-  <Slider><cite>Всегда выбирайте самый трудный путь - на нем вы не встретите конкурентов.</cite><strong>Шарль де Голль</strong></Slider>
-]
+  <Slider>
+    <cite>
+      Делай все, что можешь, там, где ты находишься, используя все, что имеешь.
+    </cite>
+    <strong>Теодор Рузвельт</strong>
+  </Slider>,
+  <Slider>
+    <cite>
+      Единственный способ сделать что-то очень хорошо – любить то, что ты
+      делаешь.
+    </cite>
+    <strong>Стив Джобс</strong>
+  </Slider>,
+  <Slider>
+    <cite>
+      Я не провалил тест. Я просто нашел сто способов написать его неправильно.
+    </cite>
+    <strong>Бенджамин Франклин</strong>
+  </Slider>,
+  <Slider>
+    <cite>Выживает не самый сильный, а самый восприимчивый к переменам.</cite>
+    <strong>Чарльз Дарвин</strong>
+  </Slider>,
+  <Slider>
+    <cite>
+      Вопрос не в том, кто мне разрешит, а в том, кто сможет мне запретить.
+    </cite>
+    <strong>Айн Рэнд</strong>
+  </Slider>,
+  <Slider>
+    <cite>
+      Неудача – это просто возможность начать снова, но уже более мудро.
+    </cite>
+    <strong>Генри Форд</strong>
+  </Slider>,
+  <Slider>
+    <cite>
+      Всегда выбирайте самый трудный путь - на нем вы не встретите конкурентов.
+    </cite>
+    <strong>Шарль де Голль</strong>
+  </Slider>,
+];
