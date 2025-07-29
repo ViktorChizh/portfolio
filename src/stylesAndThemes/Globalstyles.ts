3c5c513c8864737c599301bc5abc45d5924cf8c9
ViktorChizh <victor.chizh@mail.ru>
2025-07-29 13:25:30 +0300

    update

--- src/stylesAndThemes/Globalstyles.ts
+++ src/stylesAndThemes/Globalstyles.ts
@@ -7,39 +7,32 @@ export const GlobalStyles = createGlobalStyle`
           padding: 0;
           box-sizing: border-box;
       }
-      
       &::-webkit-scrollbar-track {
           background-color: rgb(150, 200, 250);
       }
-
       &::-webkit-scrollbar {
           width: 10px;
       }
-
       &::-webkit-scrollbar:horizontal {
           height: 10px;
       }
-
       &::-webkit-scrollbar-thumb {
           background-color: #7572D5;
           border: 3px solid rgb(150, 200, 250);
           border-radius: 10px;
       }
-
       &::-webkit-scrollbar-thumb:horizontal {
           background-color: #7572D5;
           border: 3px solid rgb(150, 200, 250);
           border-radius: 10px;
       }
   }
-
   @-moz-document url-prefix() {
     * {
         scrollbar-width: thin;
         scrollbar-color: rgb(200, 225, 250) rgb(150, 200, 250);
     }
   }
-  
   body {
       margin: 0;
       font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
@@ -49,11 +42,9 @@ export const GlobalStyles = createGlobalStyle`
       -moz-osx-font-smoothing: grayscale;
       min-width: 430px;
   }
-
   a {
       text-decoration: none;
   }
-
   li {
       list-style: none;
   }
