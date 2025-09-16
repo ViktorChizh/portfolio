59ca683cae1d2bfbad6396b18c61aa91c6056bc6
ViktorChizh <victor.chizh@mail.ru>
2025-09-16 09:56:04 +0300

    update

--- src/layout/home/Home.tsx
+++ src/layout/home/Home.tsx
@@ -60,7 +60,7 @@ export const Home = () => {
               )
             }
           >
-            {isMobile ? "резюме" : "скачайте резюме"}
+            {isMobile ? "смотреть резюме" : "скачать резюме"}
           </Button>
         </FlexWrapper>
         <PhotoWrapper>
